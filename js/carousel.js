8ab60b9d691b814f0d4163463153b4ec108b0ebd
Joe Dursun <joedursun@gmail.com>
2014-05-07 19:11:41 -0400

    Fix scrolling on hover
    - modify indententation in carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,158 +1,109 @@
 (function( $ ) {
- 	
+
     $.fn.loopScroll = function(options) {
 
-    	var settings = $.extend({
-    	  onImgClick : function() {  },
-          direction: "left",
-          speed: 60
-        }, options );
+      var settingsStuff = { onImgClick : function(){}, direction: "left", speed: 60 },
+          settings     = $.extend(settingsStuff, options),
+          locked       = {};
 
-    	var locked = { }
-    	
- 		return this.each(function() {
- 			 locked.scrollframe = $(this);
- 			 locked.scrollbox = $(this).find('.scroll_box');
-		     locked.scrollimgs = $(this).find('.r_carousel_cell');
-		     locked.scrollimgs.hide();
-
-		     $('.r_carousel_cell').bind('click touchstart', onImgClick);
-		
-
-		    $(this).children('.scroll_left').bind('touchstart mousedown mouseover', function(e){speedup('left',185); }).bind('touchend mouseup mouseout', function(e){animate();});
-		    $(this).children('.scroll_right').bind('touchstart mousedown mouseover', function(e){speedup('right',185); }).bind('touchend mouseup mouseout', function(e){animate();});
-		    $(locked.scrollbox).bind('touchstart mousedown mouseover', function(e){locked.scrollbox.stop(); }).bind('touchend mouseup mouseout', function(e){animate();});
-
-		     
-
-	        $(window).load(function() {
-	        	$('#loader').hide();
-	        	
-	        	 setWidth();
-	        	 locked.scrollimgs.fadeIn();
-	        	 animate();
-        	});
-
-	        $(window).resize(function() {
-	        	 setWidth();
-	        	 speedup();
-        	});
+      return this.each(function() {
+          locked.scrollframe = $(this);
+          locked.scrollbox = $(this).find('.scroll_box');
+          locked.scrollimgs = $(this).find('.r_carousel_cell');
+          locked.scrollimgs.hide();
+          $('.r_carousel_cell').on('click touchstart', onImgClick);
 
 
+          $(this).children('.scroll_left').on('touchstart mousedown mouseover', function(e){speedup('left',185); }).on('touchend mouseup mouseout', function(e){animate();});
+          $(this).children('.scroll_right').on('touchstart mousedown mouseover', function(e){speedup('right',185); }).on('touchend mouseup mouseout', function(e){animate();});
+          $(locked.scrollbox).on('touchstart mousedown mouseover', function(e){locked.scrollbox.stop(); }).on('touchend mouseup mouseout', function(e){animate();});
 
-	         var setWidth = function ( ){
+          $(window).load(function() {
+            $('#loader').hide();
+             setWidth();
+             locked.scrollimgs.fadeIn();
+             animate();
+          });
 
-		     	var scrollboxwidth = 0;
-	        	
-		     	locked.scrollimgs.each(function(index, el) {
-		     			var img = $(this).find('img');
-		     			//img.css('display', 'none');
-		     			//img.css('display', 'inline-block');		
-		        		scrollboxwidth += $(this).outerWidth();
-		        		console.log('scrollboxwidth: '+scrollboxwidth);
+          $(window).resize(function() {
+             setWidth();
+             speedup();
+          });
 
-	        	});
-	        	locked.scrollbox.width(scrollboxwidth+3);
-	        	console.log('scrollbox: '+locked.scrollbox.width());
-	        	console.log('scrollframe: '+locked.scrollframe.width());
-	        	//console.log('settings.direction : '+settings.direction );
+        var setWidth = function ( ){
+          var scrollboxwidth = 0;
+
+          locked.scrollimgs.each(function(index, el) {
+            var img = $(this).find('img');
+            scrollboxwidth += $(this).outerWidth();
+          });
+
+          locked.scrollbox.width(scrollboxwidth+3);
+        };
+
+        var animate = function(){
+          locked.scrollbox.stop();
+
+          if (settings.direction == "right"){
+            locked.start_y = -locked.scrollbox.width()-locked.scrollframe.width();
+            locked.end_y = 0;
+          } else if (settings.direction == "left") {
+            locked.start_y = 0;
+            locked.end_y = -locked.scrollbox.width()+locked.scrollframe.width();
+          }
+
+          var scrollframewidth = locked.scrollframe.width(),
+              distance = Math.abs(locked.end_y - parseInt(locked.scrollbox.css("left"))),
+              scrollboxAnimationOptions = {
+                duration: 500 * settings.speed,
+                easing:"linear",
+                step: function( now, fx ){
+                  locked.scrollboxanipos = now;
+                },
+                complete:  function() {
+                  locked.scrollbox.css("left", locked.start_y);
+                      animate();
+                }
+              };
+
+            locked.scrollbox.animate( {left: locked.end_y}, scrollboxAnimationOptions);
+          }
 
 
+          var speedup = function(direction,speed){
+            var scrollframewidth = locked.scrollframe.width(),
+                distance = Math.abs(locked.end_y - parseInt(locked.scrollbox.css("left"))),
+                options = {
+                  duration: 1000 * distance / speed,
+                  easing:"linear",
+                  step: function( now, fx ){
+                    locked.scrollboxanipos = now;
+                  },
+                  complete: function() {
+                    locked.scrollbox.css("left", locked.start_y);
+                    animate();
+                  }
+                };
 
-			 	 
+            locked.scrollbox.stop();
 
-		     }
+            if (direction == "right") {
+              locked.start_y = 0;
+              locked.end_y = -locked.scrollbox.width()+locked.scrollframe.width();
+            } else if (direction == "left") {
+              locked.start_y = -locked.scrollbox.width()+locked.scrollframe.width();
+              locked.end_y = 0;
+            }
 
-        	
-        	var animate = function(){
-        		locked.scrollbox.stop();
+            locked.scrollbox.animate( {left: locked.end_y}, options);
+          };
 
-        		if (settings.direction == "right") {
-			        locked.start_y -locked.scrollbox.width()-locked.scrollframe.width();
-			        locked.end_y = 0;
-			      } else if (settings.direction == "left") {
-			        locked.start_y = 0;
-			        locked.end_y = -locked.scrollbox.width()+locked.scrollframe.width();
-			      }
-
-        		var scrollframewidth = locked.scrollframe.width();
-        		var distance = Math.abs(locked.end_y - parseInt(locked.scrollbox.css("left")));
-
-        		 locked.scrollbox.animate({ 
-        		 	left: locked.end_y}, {
-				    duration: 1000 * distance / settings.speed,
-				    easing:"linear",
-				    step: function( now, fx ){
-				      locked.scrollboxanipos = now;
-				    },
-				    complete:  function() { 
-				    	locked.scrollbox.css("left", locked.start_y);
-            			animate();
-				    } 
-				  });
-        	}
-		      
-
-		    var speedup = function(direction,speed){
-		    	locked.scrollbox.stop();
-        		if (direction == "right") {
-			        locked.start_y = -locked.scrollbox.width()+locked.scrollframe.width();
-			        locked.end_y = 0;
-			      } else if (direction == "left") {
-			        locked.start_y = 0;
-			        locked.end_y = -locked.scrollbox.width()+locked.scrollframe.width();
-			      }
-
-		    	var scrollframewidth = locked.scrollframe.width();
-        		var distance = Math.abs(locked.end_y - parseInt(locked.scrollbox.css("left")));
-
-        		 locked.scrollbox.animate({ 
-        		 	left: locked.end_y}, {
-				    duration: 1000 * distance / speed,
-				    easing:"linear",
-				    step: function( now, fx ){
-				      locked.scrollboxanipos = now;
-				    },
-				    complete:  function() {
-				    	/*var newclone = locked.scrollbox.clone().addClass('sec').css('left', scrollframewidth);;
-				    	locked.scrollframe.append(newclone);
-				    	locked.scrollbox.remove();
-				    	animate2(newclone);*/
-				    	locked.scrollbox.css("left", locked.start_y);
-            			animate();
-				    } 
-				  });
-
-		    }
-
-		    
-		     var animate2 = function (elment){
-		     	var distance = Math.abs(0 - parseInt(locked.scrollbox.css("left")));
-		     	elment.animate({ 
-        		 	left: 0}, {
-				    duration: 1000 * distance / 150,
-				    easing:"linear",
-				    step: function( now, fx ){
-				      //locked.scrollboxanipos = now;
-				    },
-				    complete:  function() {
-				    	//var newclone = locked.scrollbox.clone().addClass('sec').css('left', scrollframewidth);;
-				    	//locked.scrollframe.append(newclone);
-				    	//locked.scrollbox.remove();
-				    	//locked.scrollbox.css("left", locked.start_y);
-            			//animate();
-				    } 
-				  });
-		     }
-		  
-
-		  
-		     function onImgClick(){
-		     	var index = $( ".r_carousel_cell" ).index( $(this) );
-		     	settings.onImgClick.call( index );
-			}
-   		});
+          function onImgClick(){
+            var index = $( ".r_carousel_cell" ).index( $(this) );
+            settings.onImgClick.call( index );
+          }
+        });
     };
- 
+
 }( jQuery ));
- 
+
